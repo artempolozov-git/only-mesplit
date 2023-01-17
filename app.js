878d7428cafacd6ad5fe55a84f913264c108529e
artempolozov-gitee <artem@orlovestudio.com>
2023-01-18 02:08:30 +0300

    cors fixed

--- app.js
+++ app.js
@@ -7,14 +7,24 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const cors = require('cors');
-const corsConfig = {
+/*const corsConfig = {
     origin: true,
     credentials: true,
     exposedHeaders: ['set-cookie']
+};*/
+var whitelist = ['https://only-mesplit.ru/']
+var corsOptions = {
+    origin: function (origin, callback) {
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
 };
 
 var app = express();
-app.use(cors(corsConfig));
+app.use(cors(corsOptions));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
