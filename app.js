220d4b7f053a980a5f11fe7221365663e8664830
artempolozov-gitee <artem@orlovestudio.com>
2023-01-18 02:34:14 +0300

    cors fixed

--- app.js
+++ app.js
@@ -31,11 +31,11 @@ const corsOptions = { credentials: true, origin: ["https://only-mesplit.ru/", /\
 
 var app = express();
 
-app.all('*', function (req, res) {
+app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
     res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    //...
+    next();
 });
 
 app.use(cors());
