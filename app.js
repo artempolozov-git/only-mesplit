6057307268a539e9e122ef49f37bffb65c572ba2
artempolozov-gitee <61915288+artempolozov-git@users.noreply.github.com>
2023-01-26 18:47:03 +0300

    cors fixed

--- app.js
+++ app.js
@@ -7,27 +7,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 const cors = require('cors');
-/*const corsConfig = {
-    origin: true,
-    credentials: true,
-    exposedHeaders: ['set-cookie']
-};*/
-var whitelist = ['https://only-mesplit.ru', 'http://localhost:8080/'];
-/*var corsOptions = {
-    origin: true,
-    credentials: false,
-    exposedHeaders: ['set-cookie']
-};*/
-/*var corsOptions = {
-    origin: function (origin, callback) {
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    }
-}*/
-const corsOptions = { credentials: true, origin: ["https://only-mesplit.ru/", /\.only-mesplit\.ru$/] };
 
 var app = express();
 
@@ -42,7 +21,7 @@ app.use((req, res, next) => {
         return res.sendStatus(200);
     }
     next();
-})
+});
 
 app.use(cors());
 app.use(logger('dev'));
